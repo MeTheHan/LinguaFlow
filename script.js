10e72a87c1705c426ac30e7ebc568f1b9b974a15
MeTheHan <metehanakin019@gmail.com>
2025-09-25 12:11:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Dark mode toggle fix
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
-
-// diğer fonksiyonlar (translateText, autoTranslate, swapLanguages, vs.)
-// aynen korunuyor. Burada sadece toggleDarkMode düzeltildi.
